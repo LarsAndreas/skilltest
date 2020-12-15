aa35ed1a79dc82c2eb62bc2b7a6e01a6642207b2
lve026 <lars.vestbo@student.uib.no>
2020-12-15 02:58:43 +0100

    Cleaned formating methods

--- application/app/jsonHelper.py
+++ application/app/jsonHelper.py
@@ -1,15 +1,40 @@
 class jsonHelper:
 
-    @staticmethod
-    def getIfExists(json, keys):
-        for key in keys:
-            if not key in json:
-                return "Ukjent"
-            json = json[key]
-        return json
+    unknown = "Ukjent"
+    true = "Ja"
+    false = "Nei"
 
     @staticmethod
-    def listToString(li):
-        if isinstance(li, list):
-            return ",".join(li)
-        return li
+    def getAndFormat(inp, keys):
+        for key in keys:
+
+            if inp is None:
+                return jsonHelper.unknown
+
+            if not key in inp:
+                return jsonHelper.unknown
+
+            inp = inp[key]
+
+        if isinstance(inp, bool):
+            return jsonHelper.boolFormat(inp)
+
+        if isinstance(inp, list):
+            return jsonHelper.listFormat(inp)
+
+        if isinstance(inp, str):
+            return inp
+
+        if isinstance(inp, int):
+            return inp
+
+    @staticmethod
+    def boolFormat(inp):
+        if inp:
+            return jsonHelper.true
+        else:
+            return jsonHelper.false
+
+    @staticmethod
+    def listFormat(inp):
+        return ", ".join(inp)
